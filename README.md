13f19d6b39f1c42b40acd3091da233e3e1636b0f
Chris Bowdon <1069832+cbowdon@users.noreply.github.com>
2024-09-13 13:04:12 +0100

    Initial POC

---
+++ README.md
@@ -0,0 +1,14 @@
+# AI Prompt Inversion Editor
+
+This is to demonstrate the concept that _humans_ respond well to prompts, and that we should consider whether we haven't got things completely inverted when we write a prompt and ask the LLM to complete it with information about our lives that it doesn't have.
+
+## Running this project
+
+Start a local llamafile server e.g. `./llamafile-0.8.11 Meta-Llama-3.1-8B-Instruct.Q4_K_M.gguf`.
+
+Then:
+
+```
+pnpm install
+pnpm run dev
+```
---
+++ src/App.tsx
@@ -0,0 +1,96 @@
+import { useState, useCallback } from "react";
+import _ from "lodash";
+import "./App.css";
+import spinner from "./assets/6-dots-scale-middle.svg";
+
+function App() {
+  const [text, setText] = useState<string>("");
+  const [thinking, setThinking] = useState(false);
+  const [suggestions, setSuggestions] = useState<string>("");
+
+  const fetchSuggestions = async (input: string) => {
+    try {
+      console.log("suggestion time for ", input);
+      setThinking(true);
+      setSuggestions("");
+      const response = await fetch("http://localhost:8080/completion", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({
+          prompt: input,
+          n_predict: 10,
+        }),
+      });
+      console.log(response);
+      const suggestions = await response.json();
+      console.log(suggestions);
+      setSuggestions(suggestions.content);
+      setThinking(false);
+    } catch (error) {
+      console.error("Error fetching suggestions:", error);
+    }
+  };
+
+  const callback = useCallback(_.debounce(fetchSuggestions, 500), []);
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const value = event.target.value;
+    setText(value);
+    if (
+      value.endsWith(".") ||
+      value.endsWith("?") ||
+      value.endsWith("!") ||
+      value.endsWith(",")
+    ) {
+      callback(value);
+    }
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if ((event.metaKey || event.ctrlKey) && event.key === "Enter") {
+      event.preventDefault(); // Prevent default newline behavior if necessary
+      callback(text);
+    }
+  };
+
+  return (
+    <div style={{ padding: "10px" }}>
+      <h1>AI Prompt Inversion Editor</h1>
+      <p style={{ textAlign: "justify" }}>
+        Type like usual. When you pause at the end of a sentence or hit
+        CMD+Enter, the AI will prompt you with a continuation.
+      </p>
+      <p style={{ textAlign: "justify" }}>
+        Instead of the AI finishing your sentence, which would require it to
+        know what you're thinking, it can prompt you with just enough words to
+        help you over the "what do I write next" hurdle.
+      </p>
+      <textarea
+        value={text}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+        placeholder="Start typing..."
+        style={{
+          width: "100%",
+          height: "50vh",
+          fontSize: "16px",
+          padding: "10px",
+        }}
+      />
+      <div style={{ marginTop: "10px", fontSize: "18px" }}>
+        <div>
+          <strong>Prompt:</strong>
+        </div>
+        <p>
+          {thinking && <img src={spinner} />}
+          <em>{suggestions}</em>
+        </p>
+        <p style={{ fontSize: "15px", fontWeight: "lighter" }}>
+          Hit CMD+Enter again for another suggestion.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
